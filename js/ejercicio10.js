02acf02de3060306fa747f6b9b7d934abe8c2278
Julieta Wunsche <julietawunsche@gmail.com>
2024-07-11 00:39:15 -0300

    Ejercicio 10 completo

---
+++ js/ejercicio10.js
@@ -0,0 +1,13 @@
+let numero = parseInt(prompt("Ingrese un número:"));
+if ((numero % 7) == 0) {
+    document.write("El " + numero + " es divisible por 7.");
+}
+if ((numero % 5) == 0) {
+    document.write("El " + numero + " es divisible por 5.");
+}
+if ((numero % 3) == 0) {
+    document.write("El " + numero + " es divisible por 3.");
+}
+if ((numero % 2) == 0) {
+    document.write("El " + numero + " es divisible por 2.");
+}
